6ddcd01d51993577ed636a6cb2ed8eee14ae7fca
quickMaffs44 <labios.earljustin@yahoo.com>
2022-02-15 22:42:08 -0600

    enable fps counter and exit button
    test commit

--- labcraft.py
+++ labcraft.py
@@ -21,8 +21,8 @@ pendulum_texture = load_texture('assets/mc_brick.png')
 punch_sound   = Audio('assets/punch_sound',loop = False, autoplay = False)
 block_pick = 1
 
-window.fps_counter.enabled = False
-window.exit_button.visible = False
+window.fps_counter.enabled = True
+window.exit_button.visible = True
 
 debug = False
 
