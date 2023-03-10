1f63cf16c89706c84b99c52a70465dbf769a6b06
unknown <zacharystefanich@gmail.com>
2023-03-10 16:51:08 -0600

    Added a way for the planet sim code to write out values to a csv file

--- sims.py
+++ sims.py
@@ -1,24 +1,28 @@
-rom ursina import *
+from ursina import *
 
 
 f = open("Pendulum", "w") 
 p = open("Planet", "w")
 f.write("time "+"theta "+'\n')
-time =0
+
+T=0
 def oscSim(self):
  self.t += time.dt
  self.planet.x = math.cos(self.t) + self.position.x
  self.planet.y = self.position.y
  self.planet.rotation += Vec3(0,5,0)
  self.planet.z = math.sin(self.t) + self.position.z
-            
+ global T
+ Format=self.planet.rotation[1]
+ p.write(str(T)+","+str(round(Format,2))+"\n")
+ T=T+1   
 def simple_pendulum(self):
  self.pendulum.x = self.position.x + .7
  self.pendulum.y = self.position.y
  self.pendulum.z = self.position.z
  self.pendulum.rotation += Vec3(1,0,0)
- global time
+ global T
  format=self.pendulum.rotation[0]
- f.write(str(time)+","+str(round(format,2))+"\n")
- time=time+1
+ f.write(str(T)+","+str(round(format,2))+"\n")
+ T=T+1
   
