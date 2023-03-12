979eb12efd00b7ad40cd1c5cf81aa12ece3e9f7e
unknown <zacharystefanich@gmail.com>
2023-03-11 23:31:53 -0600

    This code displays oscilation in the CSV file, but not in the simulation

--- sims.py
+++ sims.py
@@ -17,18 +17,26 @@ def oscSim(self):
  p.write(str(T)+","+str(round(Format,2))+"\n")
  T=T+1   
 def simple_pendulum(self):
- self.pendulum.x = self.position.x + .7
- self.pendulum.y = self.position.y
- self.pendulum.z = self.position.z
  global Z
- while Z<100:
+ global T
+ while Z<=100:
+  self.pendulum.x = self.position.x + .7
+  self.pendulum.y = self.position.y
+  self.pendulum.z = self.position.z
   self.pendulum.rotation += Vec3(1,0,0)
   Z=Z+1
- while Z>-100:
+  format=self.pendulum.rotation[0]/360
+  f.write(str(T)+","+str(round(format,2))+"\n")
+  T=T+1
+  Z=Z+1
+ while Z>=-100:
+  self.pendulum.x = self.position.x + .7
+  self.pendulum.y = self.position.y
+  self.pendulum.z = self.position.z
   self.pendulum.rotation += Vec3(-1,0,0)
   Z=Z-1
- global T
- format=self.pendulum.rotation[0]/360
- f.write(str(T)+","+str(round(format,2))+"\n")
- T=T+1
+  format=self.pendulum.rotation[0]/360
+  f.write(str(T)+","+str(round(format,2))+"\n")
+  T=T+1
+  Z=Z-1
   
